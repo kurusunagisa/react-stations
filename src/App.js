77c5250940a781f1f06fe53a30665ea77f39cc51
Kurusu Nagisa <kurusunagisa963@gmail.com>
2022-02-25 14:51:31 +0900

    station4

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = React.useState("https://images.dog.ceo/breeds/terrier-tibetan/n02097474_3058.jpg")
   return (<html language="ja">
     <header> <title>Dog アプリ</title></header>
-    <body><p>犬の画像を表示するサイトです</p><img src="https://images.dog.ceo/breeds/terrier-tibetan/n02097474_3058.jpg" alt="犬の画像" /></body></html>
+    <body><p>犬の画像を表示するサイトです</p><img src={dogUrl} alt="犬の画像" /></body></html>
   )
 }
