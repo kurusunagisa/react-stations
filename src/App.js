f88a3577edbadd5d065636d66270a3f35e0ea430
Kurusu Nagisa <kurusunagisa963@gmail.com>
2022-02-25 14:19:44 +0900

    station 3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
-  return (
+  return (<html language="ja">
     <header> <title>Dog アプリ</title></header>
+    <body><p>犬の画像を表示するサイトです</p><img src="https://images.dog.ceo/breeds/terrier-tibetan/n02097474_3058.jpg" alt="犬の画像" /></body></html>
   )
 }
