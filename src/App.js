8807cccd982ab4601baec69ad22d425ebb95009e
Kurusu Nagisa <kurusunagisa963@gmail.com>
2022-02-26 20:56:40 +0900

    css update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // DO NOT DELETE
 
 import * as React from 'react'
+import './Reset.css'
 import './App.css'
 
 /**
@@ -9,12 +10,12 @@ import './App.css'
  */
 export const App = () => {
   const [dogUrl, setDogUrl] = React.useState("https://images.dog.ceo/breeds/terrier-tibetan/n02097474_3058.jpg")
-  return (<html language="ja">
-    <header className="header__style"><h1>Dog アプリ</h1></header>
-    <body><p><span className="description">犬の画像を表示するサイトです</span></p>
-    <img src={dogUrl} alt="犬の画像" width="320px" height="240px"/>
-    <button onClick={() => fetch("https://dog.ceo/api/breeds/image/random").then(res => res.json()).then(result => (setDogUrl(result.message)))} className="reflesh__button">更新</button>
+  return (<div>
+    <header><h1>Dog アプリ</h1></header>
+    <p><span className="description">犬の画像を表示するサイトです</span></p>
+    <img src={dogUrl} alt="犬の画像" height="240px"/>
+    <button onClick={() => fetch("https://dog.ceo/api/breeds/image/random").then(res => res.json()).then(result => (setDogUrl(result.message)))}>更新</button>
     <hr></hr>
-    </body></html>
+    </div>
   )
 }
