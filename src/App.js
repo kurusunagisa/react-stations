2e344f96b3f27f049debe083fd1de327b96ca9c4
Kurusu Nagisa <kurusunagisa963@gmail.com>
2022-02-26 18:46:00 +0900

    rewrite css

--- src/App.js
+++ src/App.js
@@ -10,9 +10,11 @@ import './App.css'
 export const App = () => {
   const [dogUrl, setDogUrl] = React.useState("https://images.dog.ceo/breeds/terrier-tibetan/n02097474_3058.jpg")
   return (<html language="ja">
-    <header><title>Dog アプリ</title></header>
-    <body><p>犬の画像を表示するサイトです</p><img src={dogUrl} alt="犬の画像" />
-    <button onClick={() => fetch("https://dog.ceo/api/breeds/image/random").then(res => res.json()).then(result => (setDogUrl(result.message)))}>更新</button>
+    <header className="header__style"><h1>Dog アプリ</h1></header>
+    <body><p><span className="description">犬の画像を表示するサイトです</span></p>
+    <img src={dogUrl} alt="犬の画像" width="320px" height="240px"/>
+    <button onClick={() => fetch("https://dog.ceo/api/breeds/image/random").then(res => res.json()).then(result => (setDogUrl(result.message)))} className="reflesh__button">更新</button>
+    <hr></hr>
     </body></html>
   )
 }
